975d414393b2d1d55ce5f0c74f7d283e51838ef0
sam <sammybonfanti@gmail.com>
2020-09-20 17:58:19 -0600

    added animation to the score

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'B', 'B', 'B']
 const form = document.querySelector('.quiz-form')
+const result = document.querySelector('.result')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -13,5 +14,21 @@ form.addEventListener('submit', e => {
             score += 25
         }
     })
-    console.log(score);
+
+    // show result on the page
+    scrollTo(0, 0)
+    result.querySelector('span').textContent = `${score}%`
+    result.classList.remove('d-none')
+
+    // animate the numbers
+    let output = 0
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`
+        if (output === score) {
+            clearInterval(timer)
+        } else {
+            output++
+        }
+    }, 15)
+
 })
